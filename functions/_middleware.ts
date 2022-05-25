51e3f76fa8a19298bde10e2360670e3c995a07a2
Jeffrey Hunt <17629155+jeffreyhunt@users.noreply.github.com>
2022-05-25 16:55:52 +1000

    updated target for email

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,7 +3,7 @@ import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest = mailchannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "ACME Support", email: "hello@example.com" }],
+      to: [{ name: "Jeffrey Hunt", email: "jeffrey@jeffreyhunt.com.au" }],
     },
   ],
   from: { name: "Enquiry", email: "no-reply@example.com" },
